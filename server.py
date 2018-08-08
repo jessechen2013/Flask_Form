c4117c3fa1693d34fcf5dbe65a8bda932c4b1f0a
Zhixiang Chen <jessechen2013@gmail.com>
2018-08-08 14:16:16 -0700

    update

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, session, request, redirect, url_for,flash
 import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+PASS_REGEX = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}')
 app = Flask(__name__)
 app.secret_key = 'youngOG'
 
@@ -15,7 +17,7 @@ def validate():
 	lastname = request.form['lastname']
 	password = request.form['password']
 	password_confirm = request.form['password_confirm']
-	EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+	
 
 	if len(email) < 1 or len(firstname) < 1 or len(lastname) < 1 or len(password) < 1 or len(password_confirm) < 1:
 		flash("All fields are required and must not be blank")
@@ -27,8 +29,8 @@ def validate():
 	elif any(char.isdigit() for char in firstname + lastname):
 		flash("Names must not contain any numbers")
 		return redirect('/')
-	elif len(password) <= 8:
-		flash("Password must be longer than 8 characters")
+	elif not PASS_REGEX.match(password):
+		flash("Password must be longer than 8 and shorter than 10 characters; with at least one number and uppercase letter")
 		return redirect('/')
 	elif password != password_confirm:
 		flash("Please make sure password confirmation is same as the password")
